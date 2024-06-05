09189e7328f7b526a05e03db397e7aac662a3a29
Simarjeet <sjthecoder@gmail.com>
2024-06-06 04:17:42 +0530

    Upade Product and Products routes

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,46 +1,41 @@
-from fastapi import Depends, HTTPException, APIRouter
-from core.schema.products_schema import ProductReturnSchema
-from core.db_connection import get_db_session
-from sqlalchemy.orm import Session
-from core.models.products_model import Products
+from config import BASELINK
+from fastapi import HTTPException, APIRouter
+from models import Products, Inventory
 
 router = APIRouter()
 
 
-@router.get("/pants", response_model=list[ProductReturnSchema])
-def get_products(db: Session = Depends(get_db_session)):
+@router.get("/{slug}")
+async def get_product(slug: str):
     try:
-        products_data = db.query(Products).where(Products.type == "Pants").all()
-        if not products_data:
-            raise HTTPException(status_code=404, detail="No products found")
-        return products_data
+        product = await Products.get_or_none(slug=slug).prefetch_related("images")
+        if not product:
+            raise HTTPException(status_code=404, detail="Product not found")
+        response = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description,
+            "type": product.type,
+            "images": [],
+            "sizesAvailable": [],
+        }
+
+        inventory = await Inventory.filter(product_id=product.id).prefetch_related(
+            "size"
+        )
+        if inventory:
+            for i in inventory:
+                if i.quantity >= 1:
+                    response["sizesAvailable"].append(
+                        {"size": i.size.size, "available": True}
+                    )
+                else:
+                    response["sizesAvailable"].append(
+                        {"size": i.size.size, "available": False}
+                    )
+        for image in product.images:
+            response["images"].append(BASELINK + image.path)
+        return response
     except HTTPException:
         raise
-    except Exception as e:
-        raise HTTPException(status_code=500)
-
-
-@router.get("/shirt", response_model=list[ProductReturnSchema])
-def get_shirts(db: Session = Depends(get_db_session)):
-    try:
-        products_data = db.query(Products).where(Products.type == "Shirt").all()
-        if not products_data:
-            raise HTTPException(status_code=404, detail="No products found")
-        return products_data
-    except HTTPException:
-        raise
-    except Exception as e:
-        raise HTTPException(status_code=500)
-
-
-@router.get("/joggers", response_model=list[ProductReturnSchema])
-def get_joggers(db: Session = Depends(get_db_session)):
-    try:
-        products_data = db.query(Products).where(Products.type == "Joggers").all()
-        if not products_data:
-            raise HTTPException(status_code=404, detail="No products found")
-        return products_data
-    except HTTPException:
-        raise
-    except Exception as e:
-        raise HTTPException(status_code=500)
