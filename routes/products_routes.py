e430ee73f228c84355e7c1123dd5fb4918663b39
Simarjeet <sjthecoder@gmail.com>
2024-06-06 07:09:39 +0530

    Update: get_shirts function

--- routes/products_routes.py
+++ routes/products_routes.py
@@ -1,34 +1,68 @@
+from config import BASELINK
 from fastapi import HTTPException, APIRouter
 from models import Products
-# from core.schema.products_schema import ProductReturnSchema
 
 router = APIRouter()
 
 
 @router.get("/pants")
-def get_products():
+def get_products(
+    page: int = 1,
+    per_page: int = 10,
+):
+    offset = (page - 1) * per_page
     try:
-        products_data = Products.filter(type == "Pants")
+        products_data = Products.filter(type == "Pants").offset(offset).limit(per_page)
         if not products_data:
             raise HTTPException(status_code=404, detail="No products found")
         return products_data
     except HTTPException:
         raise
-    # except Exception as e:
-    #     raise HTTPException(status_code=500)
 
 
 @router.get("/shirt")
-async def get_shirts():
+async def get_shirts(
+    page: int = 1,
+    per_page: int = 12,
+):
     try:
-        products_data = await Products.filter(type="Shirt")
+        if page < 1:
+            raise HTTPException(status_code=400, detail="Page cannont be less than 0")
+        if per_page < 1:
+            raise HTTPException(status_code=400, detail="Item count be less than 0")
+        offset = (page - 1) * per_page
+        # Get the products for the current page
+        products_data = (
+            await Products.filter(type="Shirt")
+            .prefetch_related("images")
+            .offset(offset)
+            .limit(per_page)
+            # .values()
+        )
+        products = []
+        for product in products_data:
+            images = await product.images.all()
+            product_dict = dict(product)
+            image = images[0].path
+            product_dict["image"] = BASELINK + image
+            product_dict.pop("type")
+            product_dict.pop("description")
+            products.append(product_dict)
+
+        # Check if there are more products after the current page
+        next_page_products = (
+            await Products.filter(type="Shirt").offset(offset + per_page).count()
+        )
+
         if not products_data:
             raise HTTPException(status_code=404, detail="No products found")
-        return products_data
+
+        # Determine if there are more products for the next page
+        has_next_page = next_page_products > 0
+
+        return {"nextPage": has_next_page, "products": products}
     except HTTPException:
         raise
-    # except Exception as e:
-    #     raise HTTPException(status_code=500)
 
 
 @router.get("/joggers")
@@ -40,5 +74,3 @@ def get_joggers():
         return products_data
     except HTTPException:
         raise
-    # except Exception as e:
-    #     raise HTTPException(status_code=500)
