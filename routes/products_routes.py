4bd720ca6d41f94e0545f45847a5ce18d135b2e7
Simarjeet <sjthecoder@gmail.com>
2024-06-08 08:25:47 +0530

    Add: Products route

--- routes/products_routes.py
+++ routes/products_routes.py
@@ -5,8 +5,52 @@ from models import Products
 router = APIRouter()
 
 
-@router.get("/pants")
+@router.get("")
 async def get_products(
+    page: int = 1,
+    per_page: int = 16,
+):
+    try:
+        if page < 1:
+            raise HTTPException(status_code=400, detail="Page cannont be less than 0")
+        if per_page < 1:
+            raise HTTPException(status_code=400, detail="Item count be less than 0")
+        offset = (page - 1) * per_page
+        # Get the products for the current page
+        products_data = (
+            await Products.all()
+            .prefetch_related("images")
+            .offset(offset)
+            .limit(per_page)
+        )
+        products = []
+        for product in products_data:
+            images = await product.images.all()
+            product_dict = dict(product)
+            image = images[0].path
+            product_dict["image"] = BASELINK + image
+            product_dict.pop("type")
+            product_dict.pop("description")
+            products.append(product_dict)
+
+        # Check if there are more products after the current page
+        next_page_products = (
+            await Products.filter(type="Shirts").offset(offset + per_page).count()
+        )
+
+        if not products_data:
+            raise HTTPException(status_code=404, detail="No products found")
+
+        # Determine if there are more products for the next page
+        has_next_page = next_page_products > 0
+
+        return {"nextPage": has_next_page, "products": products}
+    except HTTPException:
+        raise
+
+
+@router.get("/pants")
+async def get_pants(
     page: int = 1,
     per_page: int = 16,
 ):
