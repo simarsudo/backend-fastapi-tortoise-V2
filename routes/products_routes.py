c9a62e8d6f21ba726dd7263abd71a071eb85be63
Simarjeet <sjthecoder@gmail.com>
2024-06-12 20:33:07 +0530

    Fix: Typo

--- routes/products_routes.py
+++ routes/products_routes.py
@@ -73,7 +73,7 @@ async def get_products():
 
         return {
             "shirts": res_shirts,
-            "tshirt": res_tshirts,
+            "tshirts": res_tshirts,
             "pants": res_pants,
             "joggers": res_joggers,
         }
