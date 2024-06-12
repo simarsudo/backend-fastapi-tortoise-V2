7809f5ed2d780e050e244dfbf6b4263fdea9754d
Simarjeet <sjthecoder@gmail.com>
2024-06-12 15:53:15 +0530

    Temporary inventory fix

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,6 +1,7 @@
 from config import BASELINK
 from fastapi import HTTPException, APIRouter
-from models import Products, Inventory
+from models import Products  # Inventory
+from Enum.enum_definations import ProductType
 
 router = APIRouter()
 
@@ -20,20 +21,39 @@ async def get_product(slug: str):
             "images": [],
             "sizesAvailable": [],
         }
+        sizesAvailable = []
 
-        inventory = await Inventory.filter(product_id=product.id).prefetch_related(
-            "size"
-        )
-        if inventory:
-            for i in inventory:
-                if i.quantity >= 1:
-                    response["sizesAvailable"].append(
-                        {"size": i.size.size, "available": True}
-                    )
-                else:
-                    response["sizesAvailable"].append(
-                        {"size": i.size.size, "available": False}
-                    )
+        if product.type == ProductType.JOGGERS or product.type == ProductType.PANTS:
+            sizesAvailable = [
+                {"32": True},
+                {"34": True},
+                {"36": True},
+                {"38": True},
+                {"40": True},
+            ]
+        else:
+            sizesAvailable = [
+                {"s": True},
+                {"m": True},
+                {"l": True},
+                {"xl": True},
+                {"xxl": True},
+            ]
+
+        # inventory = await Inventory.filter(product_id=product.id).prefetch_related(
+        #     "size"
+        # )
+        # if inventory:
+        #     for i in inventory:
+        #         if i.quantity >= 1:
+        #             response["sizesAvailable"].append(
+        #                 {"size": i.size.size, "available": True}
+        #             )
+        #         else:
+        #             response["sizesAvailable"].append(
+        #                 {"size": i.size.size, "available": False}
+        #             )
+        response["sizesAvailable"] = sizesAvailable
         for image in product.images:
             response["images"].append(BASELINK + image.path)
         return response
